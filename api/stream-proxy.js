72301674fb7a1247377ba9384fc8c201d5116a75
futbol-x <finest174@gmail.com>
2025-07-08 05:21:34 +0300

    Update stream-proxy.js

--- api/stream-proxy.js
+++ api/stream-proxy.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   try {
-    const streamUrl = 'http://31.220.3.103:2095/play/live.php?mac=00:1A:79:E7:32:0C&stream=47798&extension=m3u8';
+    const streamUrl = 'https://epltv1.github.io/tsn1.m3u8';
     const response = await axios.get(streamUrl, { responseType: 'stream' });
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
